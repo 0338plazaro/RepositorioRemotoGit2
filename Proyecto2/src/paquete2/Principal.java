dd76085eda4b4bafa0667192e79d290726b6b7a7
1DAM <1DAM@DESKTOP-G908T0F>
2023-04-11 18:55:27 +0200

    segundo cambio

--- Proyecto2/src/paquete2/Principal.java
+++ Proyecto2/src/paquete2/Principal.java
@@ -4,5 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 	System.out.println("Este programa muestra mensajes por pantalla");
+	
+	System.out.println("Otro cambio");
+	
 	}
 }
